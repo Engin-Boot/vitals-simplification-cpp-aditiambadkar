dd41243cfc0396898e38811b2cd018e9ed311c3c
aditiambadkar <54082235+aditiambadkar@users.noreply.github.com>
2020-08-27 11:55:16 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,6 +1,6 @@
 #include <assert.h>
 
-bool bpmIsOk(float bpm) {
+/*bool bpmIsOk(float bpm) {
   return (bpm >= 70 && bpm <= 150);
 }
 
@@ -10,10 +10,14 @@ bool spo2IsOk(float spo2) {
 
 bool respRateIsOk(float respRate) {
   return (respRate >= 30 && respRate <= 95);
+}*/
+
+bool vitalIsInLimits(int vitalValue, int lowerLimit, int upperLimit) {
+  return(vitalValue >= lowerLimit && vitalValue <= upperLimit);
 }
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  return (bpmIsOk(bpm) && spo2IsOk(spo2) && respRateIsOk(respRate));
+  return (vitalIsInLimits(bpm, 70, 150) && vitalIsInLimits(spo2, 90, 100) && vitalIsInLimits(respRate, 30, 95));
 }
 
 int main() {
